cfafea13269ac04988478e107941b8c9f3147af4
pvirie <p.virie@gmail.com>
2022-01-13 16:37:57 +0700

    up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyjs-bridge",
-    version="0.1.0",
+    version="0.1.1",
     author="Chatavut Viriyasuthee",
     author_email="chatavut@lab.ai",
     description="Vulcan's python-javascript api bridge",
